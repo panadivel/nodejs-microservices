1c735cfe6ae3ef3af0e23dbf3c9abc2955288452
pscherbinov <panadivel.v2@gmail.com>
2021-02-23 16:33:02 +0300

    change comment content depending on comment status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -4,7 +4,20 @@ const CommentList = ({ comments }) => {
 
     const renderedComments = () => {
         return comments.map(comment => {
-            return <li key={comment.id}>{comment.content}</li>
+            let content
+
+            if (comment.status === 'approved') {
+                content = comment.content
+            }
+
+            if (comment.status === 'pending') {
+                content = 'This comment is awaiting moderation'
+            }
+
+            if (comment.status === 'rejected') {
+                content = 'This comment has been rejected'
+            }
+            return <li key={comment.id}>{content}</li>
         })
     }
 
